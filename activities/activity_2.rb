6289ddfa1286e19357d84bc3eb4adf261095e065
Renier Antonio <renier.antonio@gmail.com>
2024-08-13 15:16:20 +0800

    activity 2: hash with names

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,7 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+full_name = {gName: "Renier", mName: "Fabile", sName: "Antonio"}
+
+puts "#{full_name[:gName]} #{full_name[:mName]} #{full_name[:sName]} "
