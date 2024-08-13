8462afbd1500aa204f61ec3ae547f2375c29f86b
Renier Antonio <renier.antonio@gmail.com>
2024-08-13 15:57:48 +0800

    activity 4: unique from ages

--- activities/activity_4.rb
+++ activities/activity_4.rb
@@ -2,3 +2,19 @@
 #  Write a function that takes an array of ages.
 #  The function will return an Array of ages that you could only see one time.
 #  Print on console the result.
+puts "Input limit"
+limit = gets.to_i
+ages = Array.new
+new_ages = Array.new
+i = 0
+counter = 0
+
+while i < limit
+  puts "Enter age"
+  ages.append(gets.to_i)
+  i += 1
+end
+
+new_ages = ages.uniq()
+
+puts "Unique ages #{new_ages}"
