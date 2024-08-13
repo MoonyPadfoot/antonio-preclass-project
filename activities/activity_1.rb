5b184aaf2e97ac0d002b2c825b3907259400638c
Renier Antonio <renier.antonio@gmail.com>
2024-08-13 15:09:56 +0800

    activity 1: average age of adults

--- activities/activity_1.rb
+++ activities/activity_1.rb
@@ -2,3 +2,17 @@
 #  Write a function that takes an Array of ages,
 #  the function will return the average age of adults.
 #  Print on console the result.
+
+puts "Input limit"
+limit = gets.to_i
+ages = Array.new
+i = 0
+
+while i < limit
+  ages.append(gets.to_i)
+  i += 1
+end
+
+avg = ages.sum / (ages.length)
+
+puts "average age is #{avg}"
