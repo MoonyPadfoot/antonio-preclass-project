2c06e4a08faf5fb627ff7d0c9501ce66e2c48b89
Renier Antonio <renier.antonio@gmail.com>
2024-08-13 15:00:22 +0800

    usaved file ex3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,15 +1,19 @@
 puts "Input limit"
 limit = gets.to_i
 ages = Array.new
+adult = Array.new
 i = 0
 
-while i <= limit
+while i < limit
+  puts "Enter age"
   ages.append(gets.to_i)
   i += 1
 end
 
 ages.each do |age|
   if age >= 18
-    adult = Array(adult).push(:age)
+    adult = adult.append(age)
   end
 end
+
+puts adult
