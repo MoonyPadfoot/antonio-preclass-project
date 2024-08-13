9a018b0228bdb0ad00f696c91f610a4c778b06e0
Renier Antonio <renier.antonio@gmail.com>
2024-08-13 15:33:42 +0800

    activity 3: subtracted ages

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,26 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+puts "Input limit"
+limit = gets.to_i
+ages = Array.new
+new_ages = Array.new
+i = 0
+
+while i < limit
+  puts "Enter age"
+  ages.append(gets.to_i)
+  i += 1
+end
+
+
+ages.each_with_index do |age, index|
+  if (index + 1) < ages.size
+    ans = age - ages[index + 1]
+    new_ages.append(ans)
+  end
+end
+
+puts "Subtracted ages #{new_ages}"
+
